9941c78d86eec987cabaac26fbddfc5a581dd6f8
PC <PC@DESKTOP-HS6125F>
2022-11-16 11:19:33 +0300

    Initial commit

---
+++ app/src/main/java/net/cootz/nocalc/MainActivity.java
@@ -0,0 +1,78 @@
+package net.cootz.nocalc;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.text.Editable;
+import android.view.View;
+import android.widget.EditText;
+
+import java.util.Locale;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    private float getContentValue(String text){
+        text = text.toLowerCase(Locale.ROOT);
+
+        if (text.contains("cos"))
+            return new Float(Math.cos(new Float(text.replace("cos(","").replace(")", ""))));
+        else if (text.contains("sin"))
+            return new Float(Math.sin(new Float(text.replace("sin(","").replace(")", ""))));
+        else if (text.contains("tg"))
+            return new Float(Math.tan(new Float(text.replace("sin(","").replace(")", ""))));
+        else if (text.contains("abs"))
+            return new Float(Math.abs(new Float(text.replace("sin(","").replace(")", ""))));
+        else if (text.contains("sqrt"))
+            return new Float(Math.sqrt(new Float(text.replace("sin(","").replace(")", ""))));
+        else
+            return new Float(text);
+    }
+
+    public void onAddClick(View view) {
+        float a = 0f;
+        float b = 0f;
+        boolean isBroken = false;
+
+        try {
+            a = getContentValue(((EditText)findViewById(R.id.editTextA)).getText().toString());
+            b = getContentValue(((EditText)findViewById(R.id.editTextB)).getText().toString());
+        }
+        catch (Exception e)
+        {
+            isBroken = true;
+        }
+
+        if (!isBroken)
+            ((EditText)findViewById(R.id.editTextResult)).setText(Float.toString(a+b));
+    }
+
+    public void onSubtract(View view)
+    {
+        float a = getContentValue(((EditText)findViewById(R.id.editTextA)).getText().toString());
+        float b = getContentValue(((EditText)findViewById(R.id.editTextB)).getText().toString());
+
+        ((EditText)findViewById(R.id.editTextResult)).setText(Float.toString(a-b));
+    }
+
+    public void onMultiply(View view)
+    {
+        float a = getContentValue(((EditText)findViewById(R.id.editTextA)).getText().toString());
+        float b = getContentValue(((EditText)findViewById(R.id.editTextB)).getText().toString());
+
+        ((EditText)findViewById(R.id.editTextResult)).setText(Float.toString(a*b));
+    }
+
+    public void onDivide(View view)
+    {
+        float a = getContentValue(((EditText)findViewById(R.id.editTextA)).getText().toString());
+        float b = getContentValue(((EditText)findViewById(R.id.editTextB)).getText().toString());
+
+        ((EditText)findViewById(R.id.editTextResult)).setText(Float.toString(a/b));
+    }
+}
